df376cbd4a0413999c2767affc1f55df0adf92d1
William Kemper <wkemper@openjdk.org>
2026-03-20 16:07:13 +0000

    8361726: Shenandoah: More detailed evacuation instrumentation
    Backport-of: e756c0dbbb7d99df0751d71726b173e4eabcc903

--- src/hotspot/share/gc/shenandoah/shenandoahGenerationalHeap.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahGenerationalHeap.hpp
@@ -55,7 +55,6 @@ public:
   }
 
   void print_init_logger() const override;
-  void print_tracing_info() const override;
 
   size_t unsafe_max_tlab_alloc(Thread *thread) const override;
 
@@ -66,8 +65,6 @@ private:
   ShenandoahSharedFlag  _is_aging_cycle;
   // Age census used for adapting tenuring threshold
   ShenandoahAgeCensus* _age_census;
-  // Used primarily to look for failed evacuation attempts.
-  ShenandoahEvacuationTracker*  _evac_tracker;
 
 public:
   void set_aging_cycle(bool cond) {
@@ -85,9 +82,6 @@ public:
 
   inline bool is_tenurable(const ShenandoahHeapRegion* r) const;
 
-  ShenandoahEvacuationTracker* evac_tracker() const {
-    return _evac_tracker;
-  }
 
   // Ages regions that haven't been used for allocations in the current cycle.
   // Resets ages for regions that have been used for allocations.
